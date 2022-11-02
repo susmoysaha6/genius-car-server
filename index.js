cec138cc88a06f5b606b5e711ef8bf84c077d3a5
susmoysaha6 <susmoysaha6@gmail.com>
2022-11-02 16:55:50 +0600

    order get api created

--- index.js
+++ index.js
@@ -38,11 +38,23 @@ async function run() {
 
         // orders api
 
+        app.get('/orders', async (req, res) => {
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        });
+
         app.post('/orders', async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
             res.send(result);
-        })
+        });
 
     }
     finally {
