5da1148c99e4cc6db9e4df09204d3c5cd3ec6292
susmoysaha6 <susmoysaha6@gmail.com>
2022-11-02 16:05:37 +0600

    orders post route created

--- index.js
+++ index.js
@@ -20,18 +20,28 @@ async function run() {
     try {
         const serviceCollection = client.db('geniusCar').collection('services');
 
+        const orderCollection = client.db('geniusCar').collection('orders');
+
         app.get('/services', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
-        })
+        });
 
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const service = await serviceCollection.findOne(query);
             res.send(service)
+        });
+
+        // orders api
+
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
         })
 
     }
